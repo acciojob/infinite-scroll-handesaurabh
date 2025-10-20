043e6ac0e6b438c36c8ffb60be248169f7879376
handesaurabh <handesaurabh2004@gmail.com>
2025-10-20 16:14:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
+const list = document.getElementById("infi-list");
+let itemCount = 10;
 
+for (let i = 1; i <= itemCount; i++) {
+  const li = document.createElement("li");
+  li.textContent = "Item " + i;
+  list.appendChild(li);
+}
+
+function addMoreItems() {
+  for (let i = 1; i <= 2; i++) {
+    itemCount++;
+    const li = document.createElement("li");
+    li.textContent = "Item " + itemCount;
+    list.appendChild(li);
+  }
+}
+
+list.addEventListener("scroll", function () {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addMoreItems();
+  }
+});
